e1d68671987a03ba982732bc504aa41f3c22ba9b
samuelireke <samuelireke@gmail.com>
2024-07-09 18:23:40 +0100

    Cart Page completed. Responsiveness added

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -3,7 +3,8 @@ import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } =
+    useContext(StoreContext);
   return (
     <div className="cart">
       <div className="cart-items">
@@ -36,6 +37,43 @@ const Cart = () => {
             );
           }
         })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-details">
+              <p>Subtotal</p>
+              <p>
+                {getTotalCartAmount() === 0 ? "-" : "£" + getTotalCartAmount()}
+              </p>
+            </div>
+            <hr />
+            <div className="cart-details">
+              <p>Delivery Fee</p>
+              <p>{getTotalCartAmount() === 0 ? "-" : "£2"}</p>
+            </div>
+            <hr />
+            <div className="cart-details">
+              <b>Total</b>
+              <b>
+                {getTotalCartAmount() === 0
+                  ? "-"
+                  : "£" + getTotalCartAmount() + 2}
+              </b>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a promo code, Enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder="promo code" />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
