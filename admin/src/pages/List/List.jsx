cc59def0b5ab7adaef16f1828a7be4cc017748d9
samuelireke <samuelireke@gmail.com>
2024-07-13 01:07:05 +0100

    admin page add page completed

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 import "./List.css";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 const List = () => {
+  const url = "http://localhost:4000";
+
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/all`);
+    console.log(response.data);
+    if (response.data.success) {
+      setList(response.data);
+    } else {
+      toast.error("Error getting food list");
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
   return <div>List</div>;
 };
 
